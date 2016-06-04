2081cf759e57d6cf8c4372ba2f5678d1d5524816
remyyounes <ryounes@gmail.com>
2016-06-04 02:10:37 -0700

    use degrees

--- src/Tile.elm
+++ src/Tile.elm
@@ -14,41 +14,31 @@ view color position =
     |> placeTile position
 
 viewTail color position direction =
-  let
-    angle =
-      case direction of
-        "Up" -> 0.0
-        "Right" -> 270.0
-        "Down" -> 180.0
-        "Left" -> 90.0
-        _ -> 0.0
-  in
-    filled color (polygon [(-tile / 2, tile / 2), (tile / 2, tile / 2), (0, -tile / 2)] )
-      |> rotate (angle* 3.14 / 180.0)
-      |> placeTile position
+  filled color (polygon [(-tile / 2, tile / 2), (tile / 2, tile / 2), (0, -tile / 2)] )
+    |> rotate (degrees (directionAngle direction))
+    |> placeTile position
 
 viewHead color position direction =
-  let
-    angle =
-      case direction of
-        "Up" -> 0.0
-        "Right" -> 270.0
-        "Down" -> 180.0
-        "Left" -> 90.0
-        _ -> 0.0
-  in
-    (group
-      [ filled color (circle (tile / 2) )
-      , filled color (rect tile (tile/2) ) |>  move (0, -tile / 4)
-      , filled black (circle (tile/10) ) |>  move (-tile / 4, 0)
-      , filled black (circle (tile/10) ) |>  move (tile / 4, 0)
-      ]
-    )
-      |> rotate (angle* 3.14 / 180.0)
-      |> placeTile position
+  (group
+    [ filled color (circle (tile / 2) )
+    , filled color (rect tile (tile/2) ) |>  move (0, -tile / 4)
+    , filled black (circle (tile/10) ) |>  move (-tile / 4, 0)
+    , filled black (circle (tile/10) ) |>  move (tile / 4, 0)
+    ]
+  )
+    |> rotate (degrees (directionAngle direction))
+    |> placeTile position
 
 placeTile position form =
   form
     |>  move (position.x * tile, position.y * tile)
     |>  move (tile / 2, tile / 2)
     |>  move (-world.width / 2, -world.height / 2)
+
+directionAngle direction =
+  case direction of
+    "Up" -> 0.0
+    "Right" -> 270.0
+    "Down" -> 180.0
+    "Left" -> 90.0
+    _ -> 0.0
